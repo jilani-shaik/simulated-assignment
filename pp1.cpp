9d1f7606b328651d2e9e85735dcd4f68edc99f27
jilani-shaik <62897729+jilani-shaik@users.noreply.github.com>
2020-04-04 18:43:48 +0530

    Add files via upload

---
+++ pp1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+      int a[10],i,h,k;
+      char b[10],k1;
+     for(i=0;i<10;i++)
+      b[i]=(char)(97+i);
+      printf("Enter Gifts taken by 10 students\n");
+      for(i=0;i<10;i++)
+{            
+        printf("for %c student : \t",b[i]);
+    
+      scanf("%d",&a[i]);
+}
+
